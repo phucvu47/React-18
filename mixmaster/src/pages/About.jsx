7affaf9545aa2d21c6c748f49bbde1eef341330e
phucvu47 <vuphucintern@gmail.com>
2024-02-01 18:16:41 +0700

    react router 6.4+ and react query

---
+++ mixmaster/src/pages/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Wrapper from '../assets/wrappers/AboutPage';
+
+const About = () => {
+  return (
+    <Wrapper>
+      <h3>About us</h3>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus ea
+        perspiciatis tempore tempora quaerat magnam. Eius amet aliquid nemo
+        facere ducimus rerum ea, in expedita id corrupti veniam tempora possimus
+        harum quam beatae sequi commodi deleniti dolorum alias quidem eveniet.
+        Debitis eveniet qui tenetur, animi sed reprehenderit consequuntur unde
+        ipsum.
+      </p>
+    </Wrapper>
+  );
+};
+
+export default About;
